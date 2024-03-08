abae2e581299ff0bc1b1c07006afb356be04be04
Brandon Adrián Hernández Juárez <abrandhdz@gmail.com>
2024-03-07 20:30:24 -0600

    FFLAGS compile test

--- compile.sh
+++ compile.sh
@@ -15,4 +15,4 @@ gfortran $FFLAGS -J$BIN -c $SRC/parameters.f90 -o $BIN/parameters.o
 gfortran $FFLAGS -J$BIN -c $SRC/arrays.f90 -o $BIN/arrays.o
 gfortran $FFLAGS -J$BIN -c $SRC/main_test.f90 -o $BIN/main_test.o
 gfortran $BIN/*.o -o $BIN/anharmonic_oscillator.exe
-$BIN/anharmonic_oscillator.exe
+$BIN/anharmonic_oscillator.exe <<< parameters.dat
--- src/parameters.f90
+++ src/parameters.f90
@@ -5,13 +5,22 @@ module parameters
 
   integer(i4) :: L
   real(dp) :: dt
-  character(100) :: input_file
 
   namelist /input_parameters/ L, dt
 
 contains
 
   subroutine read_input()
+
+    integer(i4) :: unit
+
+    read(*,*) input_file
+
+    open(newunit = unit, file = input_file)
+    read(unit, nml = input_parameters)
+    close(unit)
+
+    write(*, nml = input_parameter )
 
     if( L < 0) error stop "L must be > 0"
     if( dt < 0) error stop "dt must be > 0"
