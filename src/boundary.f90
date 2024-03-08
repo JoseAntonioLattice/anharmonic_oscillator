cefd88312a5639b96ccdef0c70dbc4f1e2f5f590
Hodek <hodek.mealstrom@gmail.com>
2024-03-07 22:45:02 -0600

    Function im and ip added

--- src/boundary.f90
+++ src/boundary.f90
@@ -3,57 +3,40 @@ module boundary
   use iso_fortran_env, only : dp => real64, i4 => int32
   implicit none
 
-
-
-  contains
+  integer(i4), allocatable, dimension(:) :: ip
+  integer(i4), allocatable, dimension(:) :: im
   
 
-  function ip(x, index) result(y)
-    !!This functions has an input 
-    real(dp), intent(inout), dimension(:) :: x
-    real(dp), intent(out), dimension(:) :: y
+contains
+
+  subroutine set_boundary_conditions(L)
     integer(i4) :: index
-    integer(i4) :: Rightindex, Leftindex
+    integer(i4) :: L
     
-    Rightindex=  size(x)
-    Leftindex=  1
+    allocate( ip(L), im(L) )
+    
+    do index = 1, L
+       ip(index) = index + 1
+       im(index) = index - 1
+    end do
 
-   
-    if ( index == Rightindex ) then
-      y(index) = x(Leftindex) 
-    else 
-      y(index) = x(index + 1) 
+    ip(L) = 1
+    im(1) = L
 
-    end if
-end function  ip
+  end subroutine set_boundary_conditions
 
-function im(x, index) result(y)
-  real(dp), intent(inout), dimension(:) :: x
-  real(dp), intent(out), dimension(:) :: y
-  integer(i4) :: index
-  integer(i4) :: Rightindex, Leftindex
+
   
-  Rightindex=  size(x)
-  Leftindex=  1
-
- 
-  if ( index == Leftindex ) then
-    y(index) = x(Rightindex) 
-  else 
-    y(index) = x(index - 1) 
-
-  end if
-end function  im
-
-function lagrangian(index, x,  dt) result(L) 
-  integer(i4) :: index
-  real(dp), intent(out) :: L
-  real(dp), intent(in) :: dt
-  real(dp), intent(out), dimension(:) :: x
+!function lagrangian(index, x,  dt) result(L) 
+ ! integer(i4) :: index
+  !real(dp), intent(out) :: L
+ ! real(dp), intent(in) :: dt
+ ! real(dp), intent(out), dimension(:) :: x
   
-  L = 1/2*( (x(index + 1) - x(index) ) /dt)**2 + 1/2*/(x(index))**2
-
-end function lagrangian
+ ! L = 1/2*( (x(index + 1) - x(index) ) /dt)**2 + 1/2*/(x(index))**2
+ ! L=1
+  
+!end function lagrangian
 
       
   end module boundary
