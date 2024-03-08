7a889be93c85e9c68011b6532d0e268612818663
Hodek <hodek.mealstrom@gmail.com>
2024-03-07 22:59:32 -0600

    Major corrections to im and ip. Now it is fully functional

---
+++ src/periodic_boundary_conditions.f90
@@ -0,0 +1,30 @@
+module periodic_boundary_conditions
+  !!This module contains the boundary functions
+  use iso_fortran_env, only : dp => real64, i4 => int32
+  implicit none
+
+  integer(i4), allocatable, dimension(:) :: ip
+  integer(i4), allocatable, dimension(:) :: im
+  
+
+contains
+
+  subroutine set_boundary_conditions(L)
+    integer(i4) :: index
+    integer(i4) :: L
+    
+    allocate( ip(L), im(L) )
+    
+    do index = 1, L
+       ip(index) = index + 1
+       im(index) = index - 1
+    end do
+
+    ip(L) = 1
+    im(1) = L
+
+  end subroutine set_boundary_conditions
+
+
+
+end module periodic_boundary_conditions
