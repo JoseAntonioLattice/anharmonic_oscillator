4fd704e1334f903f35b16a1ef00646c8e2f816ad
Brandon Adrián Hernández Juárez <abrandhdz@gmail.com>
2024-03-07 21:21:45 -0600

    Delete main_test compile

--- compile.sh
+++ compile.sh
@@ -13,6 +13,5 @@ fi
 
 gfortran $FFLAGS -J$BIN -c $SRC/parameters.f90 -o $BIN/parameters.o
 gfortran $FFLAGS -J$BIN -c $SRC/arrays.f90 -o $BIN/arrays.o
-gfortran $FFLAGS -J$BIN -c $SRC/main_test.f90 -o $BIN/main_test.o
 gfortran $BIN/*.o -o $BIN/anharmonic_oscillator.exe
 $BIN/anharmonic_oscillator.exe <<< parameters.dat
