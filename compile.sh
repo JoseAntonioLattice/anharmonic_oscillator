2b01e4c873eaf90b6505212ed540790ee3e9fc7c
Hodek <hodek.mealstrom@gmail.com>
2024-03-12 20:20:31 -0600

    Added module lagrangian to compile

--- compile.sh
+++ compile.sh
@@ -16,6 +16,7 @@ fi
 $FC $FFLAGS -J$BIN -c $SRC/parameters.f90 -o $BIN/parameters.o
 $FC $FFLAGS -J$BIN -c $SRC/arrays.f90 -o $BIN/arrays.o
 $FC $FFLAGS -J$BIN -c $SRC/starts.f90 -o $BIN/starts.o
+$FC $FFLAGS -J$BIN -c $SRC/euclidean_lagrangian.f90 -o $BIN/euclidean_lagrangian.f90
 $FC $FFLAGS -J$BIN -c $SRC/main.f90 -o $BIN/main.o
 $FC $BIN/*.o -o $BIN/$TARGET
 $BIN/$TARGET <<< parameters.dat
